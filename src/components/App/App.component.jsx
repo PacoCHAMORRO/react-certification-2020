6485b3122628683401e98e6a9a9e3de88a8d5af1
PacoCHAMORRO <pacochamfi@gmail.com>
2021-03-23 13:09:17 -0600

    Add useVideo hook to call Youtube API

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -4,22 +4,26 @@ import Layout from '../Layout/Layout.component';
 import Toolbar from '../Toolbar/Toolbar';
 import VideoCard from '../VideoCard/VideoCard';
 
-import { videosData } from '../data/videosData';
+import { useVideo } from '../../utils/hooks/useVideo';
 
 function App() {
+  const videosData = useVideo();
+
   return (
     <Layout>
       <Toolbar />
-      {videosData.items.map((video) => {
-        return (
-          <VideoCard
-            thumbnail={video.snippet.thumbnails.high.url}
-            title={video.snippet.title}
-            description={video.snippet.description}
-            key={video.id.videoId ? video.id.videoId : video.id.channelId}
-          />
-        );
-      })}
+      {videosData
+        ? videosData.items.map((video) => {
+            return (
+              <VideoCard
+                thumbnail={video.snippet.thumbnails.high.url}
+                title={video.snippet.title}
+                description={video.snippet.description}
+                key={video.id.videoId ? video.id.videoId : video.id.channelId}
+              />
+            );
+          })
+        : null}
     </Layout>
   );
 }
